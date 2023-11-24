7bad2d4c663344bfd007091441d7be8a44974a20
Mann14 <mannmittal622@gmail.com>
2023-11-24 19:38:44 +0530

    ejs setup

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,8 @@ const homeController = require('../controllers/home_controller');
 
 console.log("Router loaded");
 
-router.get('/',homeController.home);
+router.get('/',homeController.home);//controll home request 
+router.use('/users',require('./users'));//controll all user requestsy
+//for any further routes access from here
+// router.use('/lawyer',require('./lawyer'));
 module.exports = router;
