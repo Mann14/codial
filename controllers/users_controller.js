51f8744070bce71dda6f1c09f449e7d3e5785c0e
Mann14 <mannmittal622@gmail.com>
2023-11-26 17:58:30 +0530

    sign-up changes

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,63 @@
+const user = require('../models/user')
+
+
 module.exports.profile = function(req,res){
-    res.end('<h1>User profile</h1>');
+    return res.render('home',{
+        title:"Mann"
+    })
+}
+
+//render sign up page
+
+module.exports.signUp = (req,res)=>{
+    return res.render('user_sign_up',{
+        title:"Codial | Sign Up"
+    })
+}
+
+// Render sign in page
+
+module.exports.signIn = (req,res)=>{
+    return res.render('user_sign_in',{
+        title:"Codial | Sign In"
+    })
+}
+
+//get the sign-up data
+
+module.exports.create = (req,res)=>{
+    if(req.body.password!= req.body.confirm_password){
+        return res.redirect('back');
+    }
+    const userData = req.body;
+    user.findOne({ email: userData.email })
+    .then(existingUser => {
+      if (existingUser) {
+        // Handle existing user
+        // res.status(400).json({ message: 'User already exists' });
+        return res.redirect('back')
+      } else {
+        // Create a new user
+        return user.create(userData)
+          .then(newUser => {
+            // Handle newly created user
+            // res.status(200).json(newUser);
+            return res.redirect('/users/sign-in')
+          })
+          .catch(err => {
+            // Handle error during user creation
+            res.status(500).json({ message: 'Error creating user', error: err });
+          });
+      }
+    })
+    .catch(err => {
+      // Handle error in finding existing user
+      res.status(500).json({ message: 'Error finding user', error: err });
+    });
+}
+
+
+//sign in and create a session for user
+module.exports.createSession = (req,res)=>{
+    //TODO later
 }
--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const express = require('express');
+const cookieParser= require('cookie-parser');
 const app = express();
-const port = 8000;
+const port = 7000;
+const db = require('./config/mongoose');
 
+app.use(express.urlencoded());
+app.use(cookieParser());
+//ejs setup
 app.set('view engine','ejs');
 app.set('views','./views');
 
+//router setup
 app.use('/',require('./routes'));
+app.use(express.static('./assets'))
 
 app.listen(port,(err)=>{
     if(err){
